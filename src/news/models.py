6eb461a9f77becfcca82800de735182055bde84e
philip413 <philip413.33@gmail.com>
2016-06-17 00:12:20 -0700

    make news posting models

--- src/news/models.py
+++ src/news/models.py
@@ -3,18 +3,12 @@ from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=255)
-    #slug = models.SlugField(unique=True, max_length=255)
-    description = models.CharField(max_length=255)
+    title = models.CharField(max_length=120)
     content = models.TextField()
-    published = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
     
-    class Meta:
-       	ordering = ['-created']
-    
-    def __unicode__(self):
-       return u'%s' % self.title
- 
-    #def get_absolute_url(self):
-    #    return reverse('blog.views.post', args=[self.slug])
+
+    def __str__(self):
+        return self.title
--- src/news/views.py
+++ src/news/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def posts_home(request):
+	return HttpResponse("<h1>Hello</h1>")
