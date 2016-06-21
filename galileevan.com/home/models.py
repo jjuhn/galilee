ce0f1339822efa1948780085f183edb6378d0747
root <root@galileevan.com>
2016-06-21 16:08:41 -0400

    fixed admin posting issues

--- galileevan.com/home/models.py
+++ galileevan.com/home/models.py
@@ -10,5 +10,8 @@ class SignUp(models.Model):
 	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)  # once it creates save time
 	updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
+	def __unicode__(self):
+		return self.email
+
 	def __str__(self):  #Python 3.3 __str__
 		return self.email
