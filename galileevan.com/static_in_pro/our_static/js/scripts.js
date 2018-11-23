bf97bf9d65d8cb2ba80fb1fecea70edcd9f17177
philip413 <philip413.33@gmail.com>
2018-11-22 22:37:10 -0800

    add adult cell group singapore north korea

---
+++ galileevan.com/static_in_pro/our_static/js/scripts.js
@@ -0,0 +1,10 @@
+// // scripts.js
+
+// $(window).on("resize", function(){
+//     if($(window).width() < 768){
+//         $('.navbar-brand').find("img").attr("src","{% static 'img/galilee_logo_170x98.png' %}");
+//     }
+//     else{
+//         $('.navbar-brand').find("img").attr("src","{% static 'img/galilee-logo-cut-front.png' %}");
+//     }
+//   })
