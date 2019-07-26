509f92240f53e0346e99c4915079168db75a81bf
philip413 <philip413.33@gmail.com>
2019-07-25 23:47:00 -0400

    remove python 3

--- galileevan.com/posts/templatetags/urlify.py
+++ galileevan.com/posts/templatetags/urlify.py
@@ -1,11 +1,11 @@
 import os, sys
 
-# from urllib import quote_plus
+from urllib import quote_plus
 
-if sys.platform.startswith('linux'):
-	from urllib import quote_plus
-else:
-	from urllib.parse import quote_plus
+#if sys.platform.startswith('linux'):
+#	from urllib import quote_plus
+#else:
+#	from urllib.parse import quote_plus
 
 from django import template
 
